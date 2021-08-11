428a4eba9d9b562268e0a65b99b16e07016335a1
Mike Wall <mewall@lanl.gov>
2021-08-11 10:16:11 -0400

    Modify spock magma debug script

--- scripts/debug_spock_magma.sh
+++ scripts/debug_spock_magma.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+TMPDIR=$(mktemp -p . -d)
+pushd ${TMPDIR}
 git clone https://github.com/lanl/bml.git
-git checkout spock_magma
 cd bml
+git checkout spock_magma
 source scripts/setenv_spock_magma.sh
 bash scripts/build_spock_magma_gcc.sh
 pushd build
-srun -A csc304 -t 00:30:00 -p ecp -N 1 -n 1 --ntasks-per-node=1 ./tests/C-tests/bml-test -n inverse -t dense -p single_real
+srun -A csc304 -t 00:30:00 -p ecp -N 1 -n 1 --ntasks-per-node=1 ./tests/C-tests/bml-test -n inverse -t dense -p double_real
 popd
